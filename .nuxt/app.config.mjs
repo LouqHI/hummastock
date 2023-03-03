275dd717dcc4ab7d3e61f80e88446b38b923155c
ZenatiSarah <62511328+ZenatiSarah@users.noreply.github.com>
2023-03-03 21:21:56 +0100

    ajout du dossier pages et page home

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from 'C:/Users/admin/Documents/hummaction Offi/hummastock/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/admin/Documents/hummaction%20Offi/hummastock/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
