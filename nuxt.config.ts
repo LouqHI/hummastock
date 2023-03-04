50ccffcc1de74c7c5a616fc8668cc0aa58a0130b
ZenatiSarah <62511328+ZenatiSarah@users.noreply.github.com>
2023-03-04 09:17:57 +0100

    composant connexion ajouté

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
 
+
+export default defineNuxtConfig({
+    css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css',],
+  build: {
+    transpile: ['vuetify'],
+  },
     nitro:{
         plugins:["~/server/index.ts"]
     },
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,28 @@
 <template>
   <div>
-    <h1>Page Home</h1>
+    <Connexion />
   </div>
 </template>
+
+
+<script>
+/*
+import { storeToRefs } from "pinia";
+import { useLoginStore } from "~/stores/login";*/
+import Connexion from "../components/Connexion.client.vue";
+
+export default {
+  components: {
+    Connexion,
+  },
+  mounted() {
+    console.log("Mon app index");
+    /*const loginStore = useLoginStore();
+    const { user: authUser } = storeToRefs(loginStore);
+
+    if (authUser != -1) {
+      console.log("Partir vers les produits");
+    }*/
+  },
+};
+</script>
