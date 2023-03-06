082cc2a163479fe11d55c2a5df47091074b33477
LouqHI <h.islamiya75@gmail.com>
2023-03-06 10:05:11 +0100

    gitignore

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'C:/Users/admin/Documents/hummaction Offi/hummastock/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///C:/Users/admin/Documents/hummaction%20Offi/hummastock/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
