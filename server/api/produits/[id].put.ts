4f110ccb8b75cbf2aee4d10fdd58ecc7e2716946
LouqHI <h.islamiya75@gmail.com>
2023-03-31 19:15:53 +0200

    changement de statut de réponse pour le put

--- server/api/produits/[id].put.ts
+++ server/api/produits/[id].put.ts
@@ -9,25 +9,10 @@ export default defineEventHandler(async (event) => {
     await Produit.findByIdAndUpdate(produitId,body);
     return {message: "Ce produit a été mis à jour"}
   } catch (e){
-    throw createError({ statusCode: 500, statusMessage:"Impossible de mettre à jour le produit"})
+    throw createError({ statusCode: 400, statusMessage:"Impossible de mettre à jour le produit"})
   }
  
 });
 
 
 
-/**
- *  const urlSearchParams = new URLSearchParams(event.url.split('?')[1]);
-  const id = urlSearchParams.get('id');
-  const produit = await Produit.findOneAndUpdate({ _id: id }, body, { new: true });
-  console.log(produit);
-  return { body: produit };
- */
-/**
- * 
- * exports.updateProduit = (req, res, next) => {
-    Produit.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-        .catch(error => res.status(400).json({ error }));
-}
- */
