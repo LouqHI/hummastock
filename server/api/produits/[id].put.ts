4c5af2ad715684f5a320c087082204903600eaa9
LouqHI <h.islamiya75@gmail.com>
2023-03-31 18:58:25 +0200

    SERVER - put req pour un produit

--- server/api/produits/[id].put.ts
+++ server/api/produits/[id].put.ts
@@ -1,14 +1,28 @@
-import Produit from "~~/server/models/Produit.model";
 
-export default defineEventHandler(
-    async (event)=>{
-        const body = await readBody(event)
-        const produit= Produit.updateOne()
-        console.log(produit)
+import Produit from '~~/server/models/Produit.model';
 
-    }
-)
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+  const produitId= event.context?.params?.id;
 
+  try {
+    await Produit.findByIdAndUpdate(produitId,body);
+    return {message: "Ce produit a été mis à jour"}
+  } catch (e){
+    throw createError({ statusCode: 500, statusMessage:"Impossible de mettre à jour le produit"})
+  }
+ 
+});
+
+
+
+/**
+ *  const urlSearchParams = new URLSearchParams(event.url.split('?')[1]);
+  const id = urlSearchParams.get('id');
+  const produit = await Produit.findOneAndUpdate({ _id: id }, body, { new: true });
+  console.log(produit);
+  return { body: produit };
+ */
 /**
  * 
  * exports.updateProduit = (req, res, next) => {
